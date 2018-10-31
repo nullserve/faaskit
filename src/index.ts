574a8c594a0b6d69d035b4036b1cf6eb436934a2
David J. Felix <felix.davidj@gmail.com>
2018-10-31 16:53:30 -0400

    fix: update the typings for compose

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,23 @@ export type Handler<TEvent = any, TResult = any> = (
   context: Context,
 ) => Promise<TResult>
 
-export type Middleware = (next: Handler) => Handler
+export type Middleware<TEvent, TResult, TNextIn = any> = (
+  next: Handler<TNextIn, TResult>,
+) => Handler<TEvent, TNextIn>
 
-const compose = (...middlewares: Middleware[]) => {
-  return middlewares.reduce(
-    (a: Middleware, b: Middleware) => (next: Handler) => a(b(next)),
-    (next: Handler) => next,
-  )
+export type ComposeReduceFunc<TEvent = any, TResult = any, TNextEvent = any> = (
+  a: Middleware<TEvent, TNextEvent>,
+  b: Middleware<TNextEvent, TResult>,
+) => Middleware<TEvent, TResult>
+
+export function compose<TEvent, TResult>(
+  ...middlewares: Middleware<any, any>[]
+): Middleware<TEvent, TResult> {
+  const callbackFn: ComposeReduceFunc = (a, b) => next => a(b(next))
+  const initialValue: Middleware<TEvent, TResult> = (
+    next: Handler<TEvent, TResult>,
+  ) => next
+  return middlewares.reduce(callbackFn, initialValue)
 }
 
 export default compose
