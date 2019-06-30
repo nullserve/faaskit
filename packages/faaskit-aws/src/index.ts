98289ef5e2e0a673ee3391cdaf1102ce4bf6b486
David J. Felix <felix.davidj@gmail.com>
2019-06-29 23:54:20 -0400

    wip: begin work on an aws context adapter

---
+++ packages/faaskit-aws/src/index.ts
@@ -0,0 +1,44 @@
+import {Context as LambdaContext, Handler as LambdaHandler} from 'aws-lambda'
+
+import {Handler as FaasHandler} from '@nullserve/faaskit-core'
+import {createContext} from '@nullserve/faaskit-context'
+
+export type AWSMiddleware<
+  TEvent,
+  TResult,
+  TNextEvent = any,
+  TNextResult = any
+> = (
+  next: LambdaHandler<TNextEvent, TNextResult>,
+) => LambdaHandler<TEvent, TResult>
+
+export type AWSMiddlewareStack<
+  TEvent,
+  TResult,
+  TNextEvent = any,
+  TNextResult = any
+> = AWSMiddleware<TEvent, TResult, TNextEvent, TNextResult>
+
+export type AWSFaasMiddlewareAdapter<
+  TEvent,
+  TResult,
+  TNextEvent = any,
+  TNextResult = any
+> = (
+  next: FaasHandler<TNextEvent, TNextResult>,
+) => LambdaHandler<TEvent, TResult>
+
+export const Context = createContext<LambdaContext>(null)
+
+export function convertHandler<TEvent, TResult>(
+  handler: FaasHandler<TEvent, TResult>,
+): LambdaHandler<TEvent, TResult> {
+  const wrappedHandler: LambdaHandler<TEvent, TResult> = async (
+    event,
+    context,
+  ) => {
+    Context.Provider(context)
+    return handler(event)
+  }
+  return wrappedHandler
+}
