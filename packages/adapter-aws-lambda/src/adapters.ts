6782594b612582a310a49458097e57943159a845
David J. Felix <felix.davidj@gmail.com>
2021-06-15 19:49:02 -0500

    chore: update but also downgrade to lerna again

--- packages/adapter-aws-lambda/src/adapters.ts
+++ packages/adapter-aws-lambda/src/adapters.ts
@@ -15,19 +15,27 @@ export function adaptFaasKitHandlerForLambda<TEvent, TResult>(
 export function adaptLambdaHandlerForFaasKit<
   TEvent,
   TContext extends Context,
-  TResult
+  TResult,
 >(
   handler: LambdaHandler<TEvent, TResult>,
 ): FaasKitHandler<TEvent, TContext, TResult> {
   return async (event, context) =>
     new Promise((resolve, reject) => {
-      const result = handler(event, context.AWSLambda, (error, result) => {
-        if (!error) {
-          resolve(result)
-        } else {
-          reject(error)
-        }
-      })
+      const result = handler(
+        event,
+        context.AWSLambda,
+        (error?: Error | string | null, result?: TResult) => {
+          if (error === undefined || error === null) {
+            // FIXME: undefined behavior here caused by trying to wrap loose AWS Lambda typing.
+            // It's possible for a developer to say they'll callback with a value and call with undefined/null
+            // since typescript does not enforce runtime safety and we're hooking into @types/aws-lambda types.
+            // Without an error, it's not possible to know what the developer intended, so we succeed quietly.
+            resolve(result!)
+          } else {
+            reject(error)
+          }
+        },
+      )
       if (result) {
         result.then((result) => resolve(result)).catch((error) => reject(error))
       }
